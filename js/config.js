9e206f9ff11552c11278661c5772a9e41fd6e409
Jakub Chodubski <ch.jakub23@gmail.com>
2021-04-11 14:07:31 +0200

    I create config.js and modified almost all files

---
+++ js/config.js
@@ -0,0 +1,67 @@
+class ListedInput {
+    constructor(input,listItems,options){
+        this.cover = document.createElement("div");
+        this.input = input;
+        this.parent = input.parentElement;
+        this.items = listItems;
+        this.list = document.createElement("ul");
+        const defaultOptions = {
+            showOnStart: false,
+            function: null,
+            arguments: []
+        };
+        this.options = Object.assign(defaultOptions,options);
+        this.styling();
+        this.show();
+    }
+
+    styling(){
+        this.cover.classList.add("cover");
+        this.list.classList.add("input-list");
+        this.cover.classList.add("cover--listed");
+        this.parent.insertBefore(this.cover,this.input);
+        this.cover.appendChild(this.input);
+        this.cover.appendChild(this.list);
+        this.addingElements()
+    }
+
+    addingElements(){
+        this.list.innerHTML = "";
+        this.items.forEach(item=>{
+            const li = document.createElement("li");
+            li.textContent = item;
+            li.classList.add("input-list__item");
+            this.list.appendChild(li);
+        });
+    }
+
+    show(){
+            async function ShowWhileTyping(fn,args){
+                this.items = await fn(...args);
+                this.addingElements();
+                if(this.input.value.length){
+                    this.list.classList.add("input-list--show") 
+                    this.input.classList.add("input--typed");
+                } else{
+                    if(!this.options.showOnStart){
+                        this.list.classList.remove("input-list--show");
+                        this.input.classList.remove("input--typed");
+                    }
+                }
+            }
+            this.input.addEventListener("focus",e=>{
+            e.preventDefault();
+            if(this.options.showOnStart){
+                this.list.classList.add("input-list--show");
+                this.input.classList.add("input--typed");
+            }
+            this.input.addEventListener("input",ShowWhileTyping.bind(this, this.options.function,this.options.arguments));
+            });
+
+            this.input.addEventListener("blur",e=>{
+                this.list.classList.remove("input-list--show");
+                this.input.classList.remove("input--typed");
+                this.input.removeEventListener("input",ShowWhileTyping.bind(this, this.options.function,this.options.arguments));
+            });
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,58 +1,32 @@
-class ListedInput {
-    constructor(input,listItems,options){
-        this.cover = document.createElement("div");
-        this.input = input;
-        this.parent = input.parentElement;
-        this.items = listItems;
-        this.list = document.createElement("ul");
-        const defaultOptions = {
-            showOnStart: false
-        };
-        this.options = Object.assign(defaultOptions,options);
-        this.styling();
-        this.show();
-    }
+const searchField = document.querySelector(".input-cnt__field");
+// searchField.disabled = true;
 
-    styling(){
-        this.cover.classList.add("cover");
-        this.list.classList.add("input-list");
-        this.cover.classList.add("cover--listed");
-        this.items.forEach(item=>{
-            const li = document.createElement("li");
-            li.textContent = item;
-            li.classList.add("input-list__item");
-            this.list.appendChild(li);
+async function generateCountries(input){
+    const value = input.value;
+    const countries = fetch("https://www.trackcorona.live/api/countries",{
+        method: "GET",
+    })
+    .then(res=>res.json())
+    .then(res=>{
+        const sortedCountry = res.data.sort((prev,next)=>{
+            return prev.location.localeCompare(next.location);
         });
-        this.parent.insertBefore(this.cover,this.input);
-        this.cover.appendChild(this.input);
-        this.cover.appendChild(this.list);
-    }
-
-    show(){
-        
-            this.input.addEventListener("focus",e=>{
-            e.preventDefault();
-            if(this.options.showOnStart){
-                this.list.classList.add("input-list--show");
-                this.input.classList.add("input--typed");
-            }
-            });
-
-            this.input.addEventListener("blur",e=>{
-                this.list.classList.remove("input-list--show");
-                this.input.classList.remove("input--typed");
-            });
-
-            this.input.addEventListener("input",e=>{
-            if(this.input.value.length){
-                this.list.classList.add("input-list--show") 
-                this.input.classList.add("input--typed");
-            } else{
-                this.list.classList.remove("input-list--show");
-                this.input.classList.remove("input--typed");
-            }
+        const names = sortedCountry.map(country=>{
+            return country.location;
         });
-    }
+
+        const filteredNames = names.filter(word=>{            
+            return word.includes(value);
+        });
+        return filteredNames;
+    })
+    return await countries;
 }
 
-const input = document.querySelector(".input-cnt__field");
+(async ()=>{
+    const searchInput = new ListedInput(searchField, await generateCountries(searchField), {
+        showOnStart: true,
+        function: await generateCountries,
+        arguments: [searchField]
+    });
+})();
